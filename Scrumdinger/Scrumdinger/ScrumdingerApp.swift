3e964b3a527bdad6701ab12797a5f783c0c6d941
Justus-Saringer <jsaringer@gmx.de>
2023-11-04 20:42:33 +0100

    created project and finished first chapter

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Justus Saringer on 04.11.23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
