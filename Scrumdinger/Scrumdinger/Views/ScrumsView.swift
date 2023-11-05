d2337d931152ef741b7f580224a4eb2cfc474bb5
Justus-Saringer <jsaringer@gmx.de>
2023-11-05 13:35:56 +0100

    set up navigation

---
+++ Scrumdinger/Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,39 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Justus Saringer on 05.11.23.
+//
+
+import Foundation
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        
+        NavigationStack {
+            
+            List(scrums) { scrum in
+                NavigationLink(destination: Text(scrum.title)) {
+                    CardView(scrum: scrum)
+                }.listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
+            
+        }
+        
+    }
+    
+}
+
+#Preview {
+    return ScrumsView(scrums: DailyScrum.sampleData)
+}
