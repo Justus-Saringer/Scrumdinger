6464f808258e4c79afc70cdb0044a8d5ad06061d
Justus-Saringer <jsaringer@gmx.de>
2023-11-05 13:56:19 +0100

    fixed a probelm with the order

--- Scrumdinger/Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Scrumdinger/Views/DetailView.swift
@@ -14,28 +14,30 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting Info")){
+                
                 Label("Start Meeting", systemImage: "timer")
                     .font(.headline)
                     .foregroundColor(.accentColor)
+                
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) min")
+                }
+                .accessibilityElement(children: .combine)
+                
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.rawValue)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4.0)
+                }
+                .accessibilityElement(children: .combine)
+                
             }
-            
-            HStack {
-                Label("Length", systemImage: "clock")
-                Spacer()
-                Text("\(scrum.lengthInMinutes) min")
-            }
-            .accessibilityElement(children: .combine)
-            
-            HStack {
-                Label("Theme", systemImage: "paintpalette")
-                Spacer()
-                Text(scrum.theme.rawValue)
-                    .padding(4)
-                    .foregroundColor(scrum.theme.accentColor)
-                    .background(scrum.theme.mainColor)
-                    .cornerRadius(4.0)
-            }
-            .accessibilityElement(children: .combine)
             
         }
     }
