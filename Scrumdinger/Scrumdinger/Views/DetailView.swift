261753272cc14489bc85277a1fddc3881def75d2
Justus-Saringer <jsaringer@gmx.de>
2023-11-05 13:55:01 +0100

    added Meeting Info to DetailView

--- Scrumdinger/Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Scrumdinger/Views/DetailView.swift
@@ -12,7 +12,32 @@ struct DetailView: View {
     let scrum: DailyScrum
     
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        List {
+            Section(header: Text("Meeting Info")){
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+            }
+            
+            HStack {
+                Label("Length", systemImage: "clock")
+                Spacer()
+                Text("\(scrum.lengthInMinutes) min")
+            }
+            .accessibilityElement(children: .combine)
+            
+            HStack {
+                Label("Theme", systemImage: "paintpalette")
+                Spacer()
+                Text(scrum.theme.rawValue)
+                    .padding(4)
+                    .foregroundColor(scrum.theme.accentColor)
+                    .background(scrum.theme.mainColor)
+                    .cornerRadius(4.0)
+            }
+            .accessibilityElement(children: .combine)
+            
+        }
     }
 }
 
