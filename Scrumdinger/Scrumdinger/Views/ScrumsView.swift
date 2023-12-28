b506df5dae501556d4d14f66516c41bae458760e
Justus-Saringer <jsaringer@gmx.de>
2023-12-28 23:56:04 +0100

    implemented new scurm sheet and added it to scrums view

--- Scrumdinger/Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Scrumdinger/Views/ScrumsView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @State private var isPresentingNewScrumView = false
     
     var body: some View {
         
@@ -15,10 +16,15 @@ struct ScrumsView: View {
             }
             .navigationTitle("Daily Scrums")
             .toolbar {
-                Button(action: {}) {
+                Button(action: {
+                    isPresentingNewScrumView = true
+                }) {
                     Image(systemName: "plus")
                 }
                 .accessibilityLabel("New Scrum")
+            }
+            .sheet(isPresented: $isPresentingNewScrumView) {
+                NewScrumSheet(scrums: $scrums, isPresentingNewScrumView: $isPresentingNewScrumView)
             }
             
         }
